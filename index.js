72daa6261872a2911c890703ba248546bde157ed
zaynex <zaynex@126.com>
2017-08-18 21:26:36 +0800

    ADD: 支持sketch文件一建导出

--- index.js
+++ index.js
@@ -2,25 +2,38 @@ import sketch from 'sketchjs'
 import fs from 'fs'
 import handleData from './handleData'
 
-const actionSheet = "sketch/actionSheet.sketch"
-const actionSheetModel = "data/actionSheetModel.json"
-const actionSheetModelResult = "data/actionSheetModelResult.json"
+const actiontest = "sketch/actiontest.sketch"
+const actiontestModel = "data/actiontestModel.json"
+const actiontestModelResult = "data/actiontestModelResult.json"
 
-sketch.dump(actionSheet, (json) => {
-    fs.writeFile(actionSheetModel, JSON.stringify(JSON.parse(json), null, 4), (err) => {
-        if (err) console.log(err)
+// sketch.dump(actiontest, (json) => {
+//     fs.writeFile(actiontestModel, JSON.stringify(JSON.parse(json), null, 4), (err) => {
+//         if (err) console.log(err)
+//     })
+//     let data = JSON.parse(json)
+//     let { pages } = data
+//     if (pages[0].layers.length == 0 || (pages[0] && pages[0].layers && pages[0].layers.length && pages[0].layers[0]['<class>'] === 'MSSymbolInstance')) {
+//         depthFirstSearch(pages[1], handleData)
+//     } else {
+//         depthFirstSearch(pages[0], handleData)
+//     }
+// })
+
+sketch.dump(actiontest, (json) => {
+    fs.writeFile(actiontestModel, JSON.stringify(JSON.parse(json), null, 4), (err) => {
+        if(err) console.log(err)
     })
     let data = JSON.parse(json)
     let { pages } = data
-    if (pages[0].layers.length == 0 || (pages[0] && pages[0].layers && pages[0].layers.length && pages[0].layers[0]['<class>'] === 'MSSymbolInstance')) {
-        depthFirstSearch(pages[1], handleData)
-    } else {
-        depthFirstSearch(pages[0], handleData)
-    }
+    let symbols = pages[0].layers
+    let AllResult = symbols.map((symbol) => {
+        let name = symbol.name
+        return { [name] : depthFirstSearch(symbol, handleData)}
+    })
+
+    fs.writeFile(actiontestModelResult, JSON.stringify(AllResult, null, 4), 'utf8', err => { if (err) console.log(err) })
+    
 })
-
-
-let resultArr = []
 
 /**
  * 
@@ -28,7 +41,7 @@ let resultArr = []
  * @param {function} 处理逻辑
  */
 function depthFirstSearch(treeData, callback) {
-
+    let resultArr = []   
     let keyLevelStack = (treeData.layers || []).map((node) => {
         return [node, 0, 0, 0]
     }).reverse()
@@ -47,7 +60,9 @@ function depthFirstSearch(treeData, callback) {
             ]
         }
     }
-    resultArr = resultArr.filter(v => v != undefined)
-    fs.writeFile(actionSheetModelResult, JSON.stringify(resultArr, null, 4), 'utf8', err => { if (err) console.log(err) })
-    console.log(resultArr)
+    resultArr = resultArr.filter(v => v != null || v != undefined)
+    resultArr = resultArr.filter((v,i, res) => v != res[i+1])
+    // fs.writeFile(actiontestModelResult, JSON.stringify(resultArr, null, 4), 'utf8', err => { if (err) console.log(err) })
+    return resultArr
+    // console.log(resultArr)
 }
